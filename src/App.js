661e5362d98e89f21bf25f441dcda3d1cb42c7b7
lankika <lankika12@gmail.com>
2023-05-18 23:06:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+
+return (
+    <div className="App">
+      <div  className="product-page">
+        <h1>Hello World</h1>
+      </div>
+  </div>
+  );
+}
+
+export default App;
